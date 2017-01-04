4587d0be83f0664130bf1373716f8cce195c9afb
Brian Simmons <bsimmonsn@gmail.com>
2017-01-04 12:23:52 -0600

    Finished user_spec

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'bcrypt'
 gem 'therubyracer'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'shoulda-matchers', '~> 3.1'
   gem 'rspec-rails'
   gem 'byebug', platform: :mri
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,56 +1,22 @@
 require 'rails_helper'
 
-describe User do
-  it 'validates presence of email' do
-    user = User.new(email: nil)
-    expect(user).to validate_presence_of(:email)
+describe User, type: :model do
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:email) }
+  it { should have_secure_password}
+  it { should have_many(:mealplans)}
+  it { should have_many(:meals).through(:mealplans)}
+
+  it "returns the user's height as a formatted string" do
+    user = User.create(:username => 'testuser',
+      :email => '1234@test.com', :height => '150')
+    expect(user.height_formatted).to eq("12 ft. 6 in.")
+  end
+
+  it "returns the user's gender as a formatted string" do
+    user = User.create(:username => 'testuser',
+      :email => '1234@test.com', :gender => 'M')
+    expect(user.full_gender).to eq("Male")
+
   end
 end
-
-
-
-# describe User do
-#
-#   it 'is invalid without a username or email' do
-#     user = User.new
-#     expect { user.save! }.to raise_error(
-#       ActiveRecord::RecordInvalid
-#     )
-#   end
-#
-#   it 'is invalid without a username' do
-#     user = User.new(email: "testuser@testemail.com", password: "1234")
-#     expect { user.save! }.to raise_error(
-#       ActiveRecord::RecordInvalid
-#     )
-#   end
-#
-#   it 'is invalid without an email' do
-#     user = User.new(username: "Testuser", password: "1234")
-#     expect { user.save! }.to raise_error(
-#       ActiveRecord::RecordInvalid
-#     )
-#   end
-#
-#   it 'is valid with when provided email, username and password' do
-#     user = User.new(username: "Testuser", email: "testuser@testemail.com", password: "1234")
-#     expect { user.save! }.to_not raise_error
-#   end
-# end
-
-#
-#
-#   def height_formatted
-#     inches = self.height.to_i % 12
-#     feet = self.height.to_i / 12
-#     "#{feet} ft. #{inches} in."
-#   end
-#
-#   def full_gender
-#     if gender == "F"
-#       return "Female"
-#     else
-#       return "Male"
-#     end
-#   end
-# end
